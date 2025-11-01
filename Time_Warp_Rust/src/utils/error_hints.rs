f50d04619195c5e3613b6c7a8d2bc590a54fa2a3
HB <james@amiga-fan.net>
2025-10-31 21:35:45 -0500

    Fix clippy needless_range_loop warning

--- Time_Warp_Rust/src/utils/error_hints.rs
+++ Time_Warp_Rust/src/utils/error_hints.rs
@@ -154,8 +154,8 @@ fn levenshtein_distance(s1: &str, s2: &str) -> usize {
     for (i, row) in matrix.iter_mut().enumerate().take(len1 + 1) {
         row[0] = i;
     }
-    for j in 0..=len2 {
-        matrix[0][j] = j;
+    for (j, item) in matrix[0].iter_mut().enumerate().take(len2 + 1) {
+        *item = j;
     }
 
     for (i, c1) in s1.chars().enumerate() {
